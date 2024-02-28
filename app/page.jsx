dac6eba55b4f95328f7079a051eb4488363eb9c9
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-28 22:41:51 +0500

    buliding ticket card

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
+import TicketCard from "./components/TicketCard";
 const DashBoard = () => {
   return (
-    <div>
-      DashBoard
+    <div className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashBoard
+export default DashBoard;
