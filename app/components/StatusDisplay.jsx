24283248cccfc675f53ef37bfd5b03bcdc719412
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-29 18:17:33 +0500

    displaying fetched data and adding some custom styling

--- app/components/StatusDisplay.jsx
+++ app/components/StatusDisplay.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
-const StatusDisplay = () => {
+const StatusDisplay = ({ status }) => {
+  const getColor = (status) => {
+    let color = "bg-slate-700";
+    switch (status.toLowerCase()) {
+      case "done":
+        color = "bg-green-300";
+        return color;
+      case "started":
+        color = "bg-yellow-300";
+        return color;
+      case "not started":
+        color = "bg-red-400";
+        return color;
+    }
+    return color;
+  };
   return (
-    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200 cursor-pointer">
-      Done
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700  cursor-pointer ${getColor(
+        status
+      )}`}
+    >
+      {status}
     </span>
   );
 };
